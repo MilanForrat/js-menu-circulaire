f3e715a8c10093bbffeaad9e2606db6c991f8f27
milan.forrat <milan.forrat@gmail.com>
2022-03-08 15:04:08 +0100

    feat: sequential css animation on container

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const btnCircle = document.getElementsByClassName('btn-circle')[0];
+const circleMenu = document.getElementsByClassName('circle-menu')[0];
 
 btnCircle.addEventListener('click', () => {
     btnCircle.classList.toggle('menu-anim');
+    circleMenu.classList.toggle('circle-anim');
 })
